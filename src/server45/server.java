3787849ecde4a7a7f7432ea635c6c371a35502f8
alejandrordto <masterale2011@gmail.com>
2019-01-28 17:20:58 -0500

    servidor

--- src/server45/server.java
+++ src/server45/server.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package server45;
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
- * @author 2112107
+ * @author Carlos Andrés Castaneda Lozano
  */
 public class server {
-    
+
+/**
+     * Metodo main, metodo principal de la clase HttpServer.
+     * Crea un servidor y un cliente Socket, y asigna un puerto
+     * para el servidor web.
+     * 
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        Integer port;
+        try { 
+            port = new Integer(System.getenv("PORT"));
+        } catch (Exception e) {
+            port = 35000;
+        }
+        
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+        
+
+        Socket clientSocket = null;
+        while (true) {
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            solicitud multi = new solicitud(clientSocket);
+            multi.start();
+        }
+
+    }
 }
